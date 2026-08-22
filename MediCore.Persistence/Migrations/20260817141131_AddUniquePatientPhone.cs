6343e50a5d32e41878d60d32948faeb0b8edfe1c
BHARGAV-9900 <yarasibhargav07@gmail.com>
2026-08-22 10:26:36 +0530

    Add Unique Patient Phone

---
+++ MediCore.Persistence/Migrations/20260817141131_AddUniquePatientPhone.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediCore.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniquePatientPhone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_Email",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_PhoneNumber",
+                table: "Patients");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_Email",
+                table: "Patients",
+                column: "Email",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_PhoneNumber",
+                table: "Patients",
+                column: "PhoneNumber",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_Email",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_PhoneNumber",
+                table: "Patients");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_Email",
+                table: "Patients",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_PhoneNumber",
+                table: "Patients",
+                column: "PhoneNumber");
+        }
+    }
+}
